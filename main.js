4fcf732ddecc85aa443f623ed0a8798907bab842
natasha <natashanadnet@hotmail.com>
2023-10-11 01:44:48 -0300

    website using webpack

---
+++ main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const newDiv = document.createElement("div");
+newDiv.textContent = "HOLA";
+document.body.append(newDiv);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnRfcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBuZXdEaXYgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiZGl2XCIpO1xubmV3RGl2LnRleHRDb250ZW50ID0gXCJIT0xBXCI7XG5kb2N1bWVudC5ib2R5LmFwcGVuZChuZXdEaXYpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
